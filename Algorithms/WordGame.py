664614aed2795a601778765ead358b513b324abf
Lucas Gomes <48351001+lhggomes@users.noreply.github.com>
2020-03-14 18:22:24 -0300

    bug fixed

--- Algorithms/WordGame.py
+++ Algorithms/WordGame.py
@@ -4,10 +4,25 @@ words = []
 with open('Words.txt', 'r') as f:
     words = f.readlines()
 
-
 inWord = rd.randint(0, (len(words) - 1))
-drawnWord = words[inWord].replace('\n', '')
+drawnWord = words[inWord]
 print(drawnWord)
+
+word = ['_'] * len(drawnWord)
+while True:
+    letter = input("Digite uma letra Maiscula: ")
+    for i in range(len(drawnWord)):
+        if letter == drawnWord[i]:
+            word[i] = letter
+
+    if ''.join(word) == drawnWord:
+        print('You Won! Ended Game!')
+        print('A palavra é {0}'.format(drawnWord))
+        break
+    else:
+        print(''.join(word))
+
+
 
 
 
