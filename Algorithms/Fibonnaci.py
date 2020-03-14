95d1166b9cfbf9178076b17842347e77a3ed7302
Lucas Gomes <48351001+lhggomes@users.noreply.github.com>
2020-03-14 19:12:07 -0300

    Fibonnaci by 2 ways.

---
+++ Algorithms/Fibonnaci.py
@@ -0,0 +1,23 @@
+def fibonnaci(n):
+    if n == 1 or n == 2:
+        return 1
+    return fibonnaci(n-1) + fibonnaci(n-2)
+
+
+def fastFibonnaci(n):
+    if n == 1 or n == 2:
+        return 1
+
+    f1 = 1
+    f2 = 1
+
+    for i in range(3, n + 1):
+        f = f1 + f2
+        f2 = f1
+        f1 = f
+    return f1
+
+print("Fastly Calculation of Fibonnaci (For Method)")
+print(fastFibonnaci(32))
+print('Slowly Calculation of Fibonnaci (Recursion)')
+print(fibonnaci(32))
