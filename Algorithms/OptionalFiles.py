ea054a5f0940f04bbe6324fcbbe426c1c29d08aa
Lucas Gomes <48351001+lhggomes@users.noreply.github.com>
2020-03-14 23:34:38 -0300

    Commenting.

--- Algorithms/OptionalFiles.py
+++ Algorithms/OptionalFiles.py
@@ -3,5 +3,9 @@ def registEmployer (name, age, prof = "Unknow"):
     print('Age:', age)
     print('Profession: ', prof)
 
-
+'''
+The paramters who are not obligatory, must have to be declareted after the obligatory 
+'''
 registEmployer("Marcos" , 19)
+
+
