5592c9c1139649f735d31a40d812194b036a6c01
Lucas Gomes <48351001+lhggomes@users.noreply.github.com>
2020-03-15 13:29:20 -0300

    Creating the Abastract Class for Person

---
+++ Algorithms/OOP/Factory/Person.py
@@ -0,0 +1,19 @@
+class Person:
+    def __init__(self, name, age, rg):
+        self.name = name
+        self.age = age
+        self.rg = rg
+
+    def getName(self):
+        return self.name
+
+    def setNome(self, name):
+        self.name = name
+
+    def toString(self):
+        return Person
+
+
+
+
+
