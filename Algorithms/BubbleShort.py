aa7285a464c5f6bcb15144fdcfbf55cd4933386f
Lucas Gomes <48351001+lhggomes@users.noreply.github.com>
2020-03-14 23:20:37 -0300

    Optimization

--- Algorithms/BubbleShort.py
+++ Algorithms/BubbleShort.py
@@ -4,9 +4,14 @@ sizeL = len(l)
 #Bubble Short
 
 for i in range(sizeL):
+    change = False
     for j in range (1,sizeL - i):
         if l[j] < l[j-1]:
             l[j], l[j-1] = l[j-1], l[j]
+            change = True
+
+    if not change:
+        break
 
 print(l)
 
