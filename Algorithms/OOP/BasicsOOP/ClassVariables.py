e975e4fe5723bb34cdb336f542acf1e0e4f56bde
Lucas <lucas.henrique.s.go@gmail.com>
2020-03-21 11:03:06 -0300

    Name Changed

---
+++ Algorithms/OOP/BasicsOOP/ClassVariables.py
@@ -0,0 +1,18 @@
+class Employee:
+    raise_amount = 1.04
+
+    def __init__(self, firstName, lastName, pay):
+        self.first = firstName
+        self.last = lastName
+        self.pay = pay
+        self.email = firstName + '.' + lastName + "@aperam.com"
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+
+
+emp1 = Employee('Corey', 'Schafar', 5000)
+emp2 = Employee('Lucas', 'Gomes', 4560)
