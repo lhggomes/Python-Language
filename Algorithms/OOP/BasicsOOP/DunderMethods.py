ea964f73f451df7101717ef30f29487d9ced2f6c
Lucas <lucas.henrique.s.go@gmail.com>
2020-03-22 14:03:57 -0300

    DunderMethods.py

---
+++ Algorithms/OOP/BasicsOOP/DunderMethods.py
@@ -0,0 +1,7 @@
+"""
+Dunder or Magic Methods are especific procedures that can run on your code.
+For definition, we use the __ behind and after the name of the function, like the examples in sequence
+
+"""
+
+
