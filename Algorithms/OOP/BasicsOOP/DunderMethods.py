1fc26e0f9bd33a117cd52a1fb92d5f97ee7be7e2
Lucas <lucas.henrique.s.go@gmail.com>
2020-03-22 14:09:49 -0300

    Chaging an Method

--- Algorithms/OOP/BasicsOOP/DunderMethods.py
+++ Algorithms/OOP/BasicsOOP/DunderMethods.py
@@ -4,6 +4,7 @@ For definition, we use the __ behind and after the name of the function, like th
 
 """
 
+
 class Employee:
     raise_amount = 1.04
     num_emplo = 0
@@ -16,7 +17,17 @@ class Employee:
 
         self.num_emplo += 1
 
-#Using DunderMethods
+    #This Method change the __add__ function
+    def __add__(self, other):
+        return self.pay + other.pay
 
-print(int.__add__(1,2))
+
+ep1 = Employee('Lucas', 'Gomes', 5466)
+ep2 = Employee('Atillyo', 'Ramos', 4557)
+
+# Using DunderMethods
+print(int.__add__(1, 2))
 print(str.__add__('a', 'b'))
+
+print(ep1 + ep2)
+
