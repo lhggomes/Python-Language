11a14419943c0655606f4e1367f9aae62a9ea742
Lucas <lucas.henrique.s.go@gmail.com>
2020-03-22 14:04:22 -0300

    First Example DunderMethods.py

--- Algorithms/OOP/BasicsOOP/DunderMethods.py
+++ Algorithms/OOP/BasicsOOP/DunderMethods.py
@@ -4,4 +4,20 @@ For definition, we use the __ behind and after the name of the function, like th
 
 """
 
+class Employee:
+    raise_amount = 1.04
+    num_emplo = 0
 
+    def __init__(self, firstName, lastName, pay):
+        self.first = firstName
+        self.last = lastName
+        self.pay = pay
+        self.email = firstName + '.' + lastName + "@aperam.com"
+
+        self.num_emplo += 1
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
