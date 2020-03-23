37bb089aba13c2a9a6b672ae2a34a54bc6749f7b
Lucas <lucas.henrique.s.go@gmail.com>
2020-03-22 21:57:22 -0300

    Implem. One line by time

--- Algorithms/Wor Files/WorkingFiles.py
+++ Algorithms/Wor Files/WorkingFiles.py
@@ -1,4 +1,11 @@
-#Open Files
+# Open Files
 f = open('test.txt', 'r')
 print(f.name)
 f.close()
+
+# Using With Statement
+with open('test.txt', 'r') as f:
+    for line in f:
+        print(line, end='')
+# f_contents = f.readline()
+# print(f_contents)
