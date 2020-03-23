b6208bc1894fe71609a40d0847bffec81ace6a45
Lucas <lucas.henrique.s.go@gmail.com>
2020-03-22 22:07:06 -0300

    Implementation on method to read the files

--- Algorithms/Wor Files/WorkingFiles.py
+++ Algorithms/Wor Files/WorkingFiles.py
@@ -12,5 +12,12 @@ with open('test.txt', 'r') as f:
 with open('test.txt', 'r') as f:
     szread = 10
     f_contents = f.read(szread)
+    print(f_contents, end='')
+
+    f.seek(0)
+
+    f_contents = f.read(szread)
+    print(f_contents)
+
     while len(f_contents) > 0:
         print(f_contents, end='')
