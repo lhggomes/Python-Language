2204cc0c70809ed9c56b55026603ea53a1e889b7
Lucas <lucas.henrique.s.go@gmail.com>
2020-03-29 10:35:33 -0300

    Showing how the Polymorphism works.

--- Algorithms/OOP/BasicsOOP/Polymorphism.py
+++ Algorithms/OOP/BasicsOOP/Polymorphism.py
@@ -9,14 +9,18 @@ class Funcionario:
 
 
 class Gerente(Funcionario):
-    def __init__(self, nome, cpf, salario, senha, qt_funcionarios ):
-        super().__init__(nome,cpf, salario)
+    def __init__(self, nome, cpf, salario, senha, qt_funcionarios):
+        super().__init__(nome, cpf, salario)
         self._senha = senha
         self._qt_func = qt_funcionarios
 
-
+    #Override Method (Polymorphism)
     def get_bonificacao(self):
         return self._salario * 0.15
+
+    #Calling the Super Function
+    def get_bonificacao(self):
+        return super().get_bonificacao() + 1000
 
 
 gerente = Gerente('José', '222222222-22', 5000.0, '1234', 0)
