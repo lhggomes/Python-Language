c66cf8be1f90b7b6fa093bf47e7df88390de62c3
Lucas <lucas.henrique.s.go@gmail.com>
2020-03-29 21:30:26 -0300

    Creating the Abc for the Interface.py

---
+++ Algorithms/OOP/BasicsOOP/Interface.py
@@ -0,0 +1,27 @@
+import abc
+
+
+class Controlador(abc.ABC):
+    @abc.abstractmethod
+    def ligar(self):
+        pass
+
+    @abc.abstractmethod
+    def desligar(self):
+        pass
+
+    @abc.abstractmethod
+    def abrir_menu(self):
+        pass
+
+    @abc.abstractmethod
+    def fechar_menu(self):
+        pass
+
+    @abc.abstractmethod
+    def mais_volume(self):
+        pass
+
+    @abc.abstractmethod
+    def menos_volume(self):
+        pass
