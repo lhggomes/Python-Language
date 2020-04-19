211df711b29d2b0dade298f32d0addf740a23d19
Lucas <lucas.henrique.s.go@gmail.com>
2020-04-19 15:40:10 -0300

    Abstract Method isConnect

--- Algorithms/CRUD/DBConnection.py
+++ Algorithms/CRUD/DBConnection.py
@@ -1,17 +1,10 @@
 import pymysql
 import sys
+import abc
 
-
-def is_connect():
-    global status
-    if status :
-        return True
-    else:
-        return False
 
 
 class connection:
-
     def create_connection(self, host="localhost", user="root", password="", data_base="", port=3306):
 
         self._host = host
@@ -32,8 +25,16 @@ class connection:
                 self._port
                 )
             status = True
-        except :
+        except:
             print('Erro não foi possível Connectar', sys.exc_info()[0])
             status = False
 
         return con
+
+    @abc.abstractstaticmethod
+    def is_connect(self):
+        global status
+        if status :
+            return True
+        else:
+            return False
