9016076d3151ece67e010e2e90bcf357252f3c97
Lucas <lucas.henrique.s.go@gmail.com>
2020-04-19 16:03:21 -0300

    Removing the class, only function left behind

--- Algorithms/CRUD/DBConnection.py
+++ Algorithms/CRUD/DBConnection.py
@@ -1,32 +1,30 @@
 import pymysql
 import sys
-import abc
 
 
-class connection:
-    def create_connection(self, host="localhost", user="root", password="", data_base="", port=3306):
-        global con
-        global status
+def create_connection(host="localhost", user="root", password="", data_base="", port=3306) :
+    global con
+    global status
 
-        try:
-            con = pymysql.connect(
-                host,
-                user,
-                password,
-                data_base,
-                port
-                )
-            status = True
-        except:
-            print('Erro não foi possível Connectar', sys.exc_info()[0])
-            status = False
+    try:
+        con = pymysql.connect(
+            host,
+            user,
+            password,
+            data_base,
+            port
+            )
+        status = True
+    except:
+        print('Erro não foi possível Connectar', sys.exc_info()[0])
+        status = False
 
-        return con
+    return con
 
-    @abc.abstractstaticmethod
-    def is_connect(self):
-        global status
-        if status :
-            return True
-        else :
-            return False
+
+def is_connect(self) :
+    global status
+    if status:
+        return True
+    else:
+        return False
