227871a471da4a9937c99a1917f3690d3b8a239e
Lucas <lucas.henrique.s.go@gmail.com>
2020-04-19 16:13:56 -0300

    Adapting to the new Database model.

--- Algorithms/CRUD/Select.py
+++ Algorithms/CRUD/Select.py
@@ -1,13 +1,14 @@
-from Algorithms.CRUD import DBConnection
+from Algorithms.CRUD import DBConnection as Db
 
-connect = DBConnection.connection(data_base='precos')
-c = connect.create_cursor()
+connect = Db.create_connection(data_base="precos")
+cursor_db = connect.cursor()
+
 
 query = "SELECT * FROM prodmov ORDER BY numtransmov"
 
-if connect.is_connect():
-    c.execute(query)
-    result = c.fetchall()
+if Db.is_connect():
+    cursor_db.execute(query)
+    result = cursor_db.fetchall()
 
     for x in result:
         print(x)
