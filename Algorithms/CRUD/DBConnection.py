201c76b098fb043fac8e7cbcaa2f72cf113a287d
Lucas <lucas.henrique.s.go@gmail.com>
2020-04-19 13:34:45 -0300

    Using a Try Except

--- Algorithms/CRUD/DBConnection.py
+++ Algorithms/CRUD/DBConnection.py
@@ -2,8 +2,7 @@ import pymysql
 import sys
 
 
-
-class connection():
+class connection:
     def __init__(self, host='localhost', user='root', password='', data_base='', port='3306', charset='utf8mb4'):
         self._host = host
         self._user = user
@@ -21,8 +20,7 @@ class connection():
                 self._password,
                 self._data_base,
                 self._port,
-                self._charset )
-
+                self._charset)
 
         except:
             print('Erro não foi possível Connectar', sys.exc_info()[0])
@@ -30,8 +28,8 @@ class connection():
         finally:
             return con
 
-    def isConnect(self):
-        if (self.create_connection() != None):
+    def is_connect(self):
+        if self.create_connection() is not None:
             return True
         else:
             return False
