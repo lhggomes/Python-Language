f480d91f1405a823d03945d6bbd5f92d2ac26390
Lucas <lucas.henrique.s.go@gmail.com>
2020-04-19 21:17:16 -0300

    INSERT Statament

--- Algorithms/CRUD/Insert.py
+++ Algorithms/CRUD/Insert.py
@@ -4,19 +4,18 @@ import sys
 connection = Db.create_connection(data_base="precos")
 c = connection.cursor()
 
-sql = "INSERT INTO prodmov (numtransmov, codprod, qt, precoCompra, numnfent, codfornec) VALUES (%s, %s, %s, %s, %s, %s)"
-val = (4, 10104, 1423, 1, 1, 124, 3352)
+sql = "INSERT INTO prodmov (numtransmov, codprod, qt, precoCompra, numnfent, codfornec) VALUES (4, 10104, 1423, 1, 1, " \
+      "124, 3352) "
 
 try:
-    if connection.is_connect():
-        c.execute(sql, val)
-    else:
-        breakpoint()
+    c.execute(sql)
+    connection.commit()
 except:
     print('Erro não foi possivel inserir', sys.exc_info()[0])
     connection.rollback()
 finally:
-    print('Data insert successfully')
-    connection.commit()
+    c.close()
+    print("Connection Closed")
+
 
 
